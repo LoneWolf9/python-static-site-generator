c4ffde4386fc040caab6d27f605d39b6c49757f0
LoneWolf9 <lstdon9@yahoo.com>
2021-06-13 15:05:14 -0600

    updating copy2 call

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -24,7 +24,7 @@ class Parser:
             file.write(content)
     
     def copy(self, path, source, dest):
-        shutil.copy2(dest / path.relative_to(source))
+        shutil.copy2(path, dest / path.relative_to(source))
     
     
     class ResourceParser(Parser):
