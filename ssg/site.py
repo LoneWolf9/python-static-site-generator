7415d7697161c2b9b5059a9adbf2d1dc5424081a
LoneWolf9 <lstdon9@yahoo.com>
2021-06-13 13:55:20 -0600

    site.py update
    correcting mistype for rglob

--- ssg/site.py
+++ ssg/site.py
@@ -16,7 +16,7 @@ class Site:
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         
-        for path in self.source.rglog("*"):
+        for path in self.source.rglob("*"):
             if path.is_dir():
                 create_dir(path)
         
