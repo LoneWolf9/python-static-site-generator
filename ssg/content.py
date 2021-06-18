aecd592d5cc2fed85c94a6ffe9e04182285ebd7b
LoneWolf9 <lstdon9@yahoo.com>
2021-06-18 14:17:15 -0600

    update content.py

---
+++ ssg/content.py
@@ -0,0 +1,16 @@
+import re
+
+from yaml import load, FullLoader
+
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+
+    __delimiter = "^(?:-/\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
+    def load(self):
+        _ = __regex.split('', 2)
+        fm = __regex.split('', 2)
+        content = __regex.split('', 2)
