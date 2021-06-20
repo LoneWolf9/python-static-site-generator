99c519aef2d08094b0c429711ceea24199c103ac
LoneWolf9 <lstdon9@yahoo.com>
2021-06-20 08:11:47 -0600

    Update parsers.py
    adding imports for module4

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,43 +1,39 @@
 import shutil
+import sys
 
 from typing import List
-
 from pathlib import Path
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
+
 
 class Parser:
-    
+
     extensions: List[str] = []
-    
+
     def valid_extension(self, extension):
         return extension in self.extensions
-    
+
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
-    
+
     def read(self, path):
         with open(path, 'r') as file:
             return file.read()
-    
+
     def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
         with open(full_path, 'w') as file:
             file.write(content)
-    
+
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
-    
-    
+
+
 class ResourceParser(Parser):
-        
+
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
-        
+
     def parse(self, path, source, dest):
         Parser.copy(path, source, dest)
-        
-           
-            
-        
-        
-        
-    
-    
