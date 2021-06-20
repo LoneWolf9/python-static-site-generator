25bb15c123a4968ebe6c5b070d0bbff06ac07ec3
LoneWolf9 <lstdon9@yahoo.com>
2021-06-20 08:30:04 -0600

    Update ssg.py
    adding parser subclasses to parser list

--- ssg.py
+++ ssg.py
@@ -1,16 +1,18 @@
-import typer 
+import typer
 
 import ssg.parsers
 
 from ssg.site import Site
 
+
 def main(source="content", dest="dist"):
     config = {
         "source": source,
         "dest": dest,
-        "parsers": [ssg.parsers.ResourceParser(),],
+        "parsers": [ssg.parsers.ResourceParser(), ssg.parsers.MarkownParser(), ssg.parsers.ReStructuredTextParser()],
     }
-    
+
     Site(**config).build()
+
 
 typer.run(main)
